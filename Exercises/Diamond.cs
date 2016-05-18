edfed27e248e650cc16ef263e1a4e950a313b7b8
Hongmin Wang <ranwang568@gmail.com>
2016-05-18 01:17:29 -0400

    Add files via upload

---
+++ Exercises/Diamond.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise7;
+
+namespace Exercise6 {
+    class Diamond {
+        public void PrintDiamond(int row) {
+            for(int i = 0; i < row; i++) {//the upper part of diamond 
+                for(int j = 0; j < row - i; j++) { //output the white space
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++) { //output the star
+                    Console.Write("*");
+                }
+                for (int k = 0; k < i; k++) { //output the remaining star
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            for (int i = row - 2; i >= 0; i--) {//the lower part of diamond
+                for (int j = 0; j <= (row - 1) - i; j++) {//output the white space
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++) {//output the white space
+                    Console.Write("*");
+                }
+                for (int j = 0; j < i; j++) {//output the white space
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            Diamond d = new Diamond();
+            Console.WriteLine("please specify the rows of the Diamond: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Diamond shows:");
+            d.PrintDiamond(row);
+            Exercise7.ATM ert = new Exercise7.ATM();
+        }
+    }
+}
---
+++ Exercises/ExponentNum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework0516 {
+    class ExponentNum {
+        public void prompt() {
+            Console.WriteLine("By default, output the first 100 ExponentNum numbers:");
+            int range = 100;
+            OutputExponent(range);
+        }
+        public void OutputExponent(int range) {
+            if(range < 1) {//error handling
+                return;
+            }
+            int cnt = 0;
+            SortedSet<int> st = new SortedSet<int>();
+            st.Add(1);
+            while (cnt < range) {
+                st.Add(st.First() * 2);
+                st.Add(st.First() * 3);
+                Console.Write(String.Format("{0, -8}", st.First()));
+                st.Remove(st.First());
+                cnt++;
+                if (cnt % 10 == 0) {//each row output 10 nums
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void Main(string[] args) {
+            ExponentNum en = new ExponentNum();
+            en.prompt();
+        }
+    }
+}
