7c35debf9d41afaa247ba9f5dd748b514f968d06
Hongmin Wang <ranwang568@gmail.com>
2016-06-06 17:20:50 -0400

    Add files via upload

---
+++ DropDownListDemo/tables/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PageLifeCycleDemo.tables {
+    public class Department {
+        public int Dept_ID { get; set; }
+        public string Dept_Name { get; set; }
+
+        public static List<Department> popDeptData() {
+            List<Department> dept = new List<Department>();
+            dept.Add(new Department { Dept_ID = 1, Dept_Name = "Computer Engineer" });
+            dept.Add(new Department { Dept_ID = 2, Dept_Name = "Data Scientist" });
+            dept.Add(new Department { Dept_ID = 3, Dept_Name = "UI Development" });
+            dept.Add(new Department { Dept_ID = 4, Dept_Name = "Test Team" });
+            return dept;
+        }
+    }
+}
+
+
---
+++ DropDownListDemo/tables/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PageLifeCycleDemo.tables {
+    public class Employee {
+        public int Emp_ID { get; set; }
+        public string Emp_Name { get; set; }
+        public int Dept_ID { get; set; }
+        public static List<Employee> popEmpData() {
+            List<Employee> emp = new List<Employee>();
+            emp.Add(new Employee { Emp_ID = 1, Emp_Name = "Sam", Dept_ID = 1 });
+            emp.Add(new Employee { Emp_ID = 2, Emp_Name = "Mary", Dept_ID = 2 });
+            emp.Add(new Employee { Emp_ID = 3, Emp_Name = "Jack", Dept_ID = 2 });
+            emp.Add(new Employee { Emp_ID = 4, Emp_Name = "David", Dept_ID = 1 });
+            emp.Add(new Employee { Emp_ID = 5, Emp_Name = "Louis", Dept_ID = 3 });
+            emp.Add(new Employee { Emp_ID = 6, Emp_Name = "Vika", Dept_ID = 1 });
+            emp.Add(new Employee { Emp_ID = 7, Emp_Name = "Rafa", Dept_ID = 4 });
+            return emp;
+        }
+    }
+}
