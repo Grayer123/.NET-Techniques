e0286218c69eb8dbb7cbf86072777811137f67e3
Hongmin Wang <ranwang568@gmail.com>
2016-06-07 10:09:00 -0400

    Add files via upload

---
+++ CheckBox_ListBoxDemo/CheckBox_ListBox.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CheckBox_ListBox.aspx.cs" Inherits="PageLifeCycleDemo.CheckBox_ListBoxDemo.CheckBox_ListBox" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:CheckBoxList runat="server" ID="ckBoxList" RepeatDirection="Horizontal" 
+            OnSelectedIndexChanged="ckBoxList_SelectedIndexChanged" AutoPostBack="true">
+            <asp:ListItem Text="Bachelor" Value="1" />
+            <asp:ListItem Text="Graduate" Value="2" />
+            <asp:ListItem Text="Post Graduate" Value="3" />
+            <asp:ListItem Text="Doctrate" Value="4" />        
+        </asp:CheckBoxList>
+    </div>
+
+    <div>
+        <asp:ListBox runat="server" ID="lstBox" Width="200px" 
+            SelectionMode="Multiple">
+        </asp:ListBox>
+        <br />
+    </div>
+    <div>
+        <asp:Label Text="" runat="server" ID="lblSummary" Font-Bold="true" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ CheckBox_ListBoxDemo/CheckBox_ListBox.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PageLifeCycleDemo.CheckBox_ListBoxDemo {
+    public partial class CheckBox_ListBox : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void ckBoxList_SelectedIndexChanged(object sender, EventArgs e) {
+            lstBox.Items.Clear(); //Clear() first, otherwise could add the duplicated records
+            foreach(ListItem li in ckBoxList.Items) {
+                if (li.Selected) {
+                    //lstBox.Items.Add(li); //at this time li has been selected, Add() has the overloaded Add(string) version
+                    lstBox.Items.Add(li.Text);
+                }
+            }
+
+            if(ckBoxList.SelectedIndex == -1) { //no items has been selected
+                lblSummary.ForeColor = System.Drawing.Color.Red;
+            }
+            lblSummary.Text = lstBox.Items.Count.ToString() + " Item(s) Selected";
+        }
+    }
+}
