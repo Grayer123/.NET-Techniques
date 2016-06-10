35965ff6834402385f0ac56808f849a678a17ccf
Hongmin Wang <ranwang568@gmail.com>
2016-06-10 10:00:26 -0400

    Add files via upload

---
+++ Server Controls/DropDownListDemo/DropDownList.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DropDownList.aspx.cs" Inherits="PageLifeCycleDemo.WebForm3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <%--DataTextField ="Dept_Name" DataValueField="Dept_ID"--%>
+        <asp:DropDownList runat="server" ID="dropListDepts" Width="200px"             
+            AutoPostBack="true" OnSelectedIndexChanged="dropsdwnDepts_SelectedIndexChanged">
+            <%--<asp:ListItem Text="Select Dept." Value="0"></asp:ListItem>
+            <asp:ListItem Text="Computer Engineering" Value="1" Selected="True"></asp:ListItem>
+            <asp:ListItem Text="Data Scientist" Value="2" Enabled="false"></asp:ListItem>
+            <asp:ListItem Text="UI Development" Value="3"></asp:ListItem>
+            <asp:ListItem Text="Test Team" Value="4"></asp:ListItem>--%>
+        </asp:DropDownList>
+        <br />
+    </div>
+
+    <div>
+        <asp:DropDownList runat="server" ID="dropListEmpts" Width="200px">
+            <%--<asp:ListItem Text="Select Dept." Value="0"></asp:ListItem>
+            <asp:ListItem Text="Computer Engineering" Value="1" Selected="True"></asp:ListItem>
+            <asp:ListItem Text="Data Scientist" Value="2" Enabled="false"></asp:ListItem>
+            <asp:ListItem Text="UI Development" Value="3"></asp:ListItem>
+            <asp:ListItem Text="Test Team" Value="4"></asp:ListItem>--%>
+        </asp:DropDownList>
+    </div>
+    </form>
+</body>
+</html>
+
+
+
+
+
+
+        
---
+++ Server Controls/DropDownListDemo/DropDownList.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PageLifeCycleDemo.tables;
+
+
+namespace PageLifeCycleDemo {
+    public partial class WebForm3 : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            if (!Page.IsPostBack) {
+                dropListDepts.DataSource = Department.popDeptData();
+                dropListDepts.DataTextField = "Dept_Name";
+                dropListDepts.DataValueField = "Dept_ID";
+                dropListDepts.DataBind();
+                //add the default record to the Department table
+                ListItem liDept = new ListItem("Select Dept.", "0");
+                dropListDepts.Items.Insert(0, liDept); //insert it to the beginning 
+
+
+                ListItem liEmp = new ListItem("Select Employee.", "0");
+                dropListEmpts.Items.Insert(0, liEmp);
+                dropListEmpts.Enabled = false;
+            }
+            
+        }
+
+        protected void dropsdwnDepts_SelectedIndexChanged(object sender, EventArgs e) {
+            if(dropListDepts.SelectedIndex != 0) {
+                dropListEmpts.Enabled = true;
+                int val = Convert.ToInt32(dropListDepts.SelectedValue);
+
+                dropListEmpts.DataSource = Employee.popEmpData().FindAll(x => x.Dept_ID == val);
+                dropListEmpts.DataTextField = "Emp_Name";
+                dropListEmpts.DataValueField = "Emp_ID";
+                dropListEmpts.DataBind();
+                ListItem liEmp = new ListItem("Select Employee.", "0");
+                dropListEmpts.Items.Insert(0, liEmp);
+            }
+            else {
+                dropListEmpts.SelectedIndex = 0;
+                dropListEmpts.Enabled = false;
+            }
+        }
+    }
+}
+
+
+
+
