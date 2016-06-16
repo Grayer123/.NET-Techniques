a01ba4233e3391210c832c34ea088c0ddee83cfa
Hongmin Wang <ranwang568@gmail.com>
2016-06-15 20:55:23 -0400

    Add files via upload

---
+++ MultiWCFService/MultiWCFService/CompanyService.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MultiWCFService {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class CompanyService : ICompanyPublicService, ICompanyConfidentialService {
+        public string GetConfidentialInfo() {
+            return "This is confidential info and only available over TCP behind the company FireWall";
+        }
+
+        public string GetPublicInfo() {
+            return "This is public info and available over HTTP to all general public outside the FireWall";
+        }
+    }
+}
---
+++ MultiWCFService/WebClient1/Client1.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebClient1 {
+    public partial class Client1 : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            CompanyService.CompanyPublicServiceClient client1 = 
+                new CompanyService.CompanyPublicServiceClient("BasicHttpBinding_ICompanyPublicService");
+            Response.Write(client1.GetPublicInfo() + "<br />");
+
+            CompanyService.CompanyConfidentialServiceClient client2 =
+                new CompanyService.CompanyConfidentialServiceClient("WSHttpBinding_ICompanyConfidentialService");
+            Response.Write(client2.GetConfidentialInfo());
+        }
+    }
+}
